9c8d62fcfffb86acef2e5c545b45017e9b09e9ac
Youngleechen <137005469+Youngleechen@users.noreply.github.com>
2025-12-02 17:00:23 +0000

    d

---
+++ src/app/test/page.tsx
@@ -0,0 +1,9 @@
+export default function TestPage() {
+  return (
+    <main style={{ padding: '2rem', textAlign: 'center' }}>
+      <h1>🧪 Test Page Works!</h1>
+      <p>This is https://editwrite.vercel.app/test</p>
+      <p>If you see this, your routing is perfect!</p>
+    </main>
+  );
+}
